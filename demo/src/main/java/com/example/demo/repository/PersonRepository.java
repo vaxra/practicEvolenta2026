5f0b9b0ed32ad959bb14af55de38263f4dbef410
vaxra <luzin2005@icloud.com>
2026-06-17 17:23:53 +0300

    Delete demo directory

--- demo/src/main/java/com/example/demo/repository/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.dto.Person;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonRepository extends CrudRepository<Person, Integer> {
-}
