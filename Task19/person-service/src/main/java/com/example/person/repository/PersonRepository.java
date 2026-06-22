e177432eee6ae324d1bf2f27b3605ab8b8ae1c04
vaxra <luzin2005@icloud.com>
2026-06-22 09:49:46 +0300

    Delete Task19 directory

--- Task19/person-service/src/main/java/com/example/person/repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.person.repository;
-
-import com.example.person.dto.Person;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PersonRepository extends CrudRepository<Person, Integer> {
-}
